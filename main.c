c24b2604b123ae03a155006e6d584cdfc63a4caf
otodrakela <113287800+otodrakela@users.noreply.github.com>
2023-07-05 17:52:58 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int t1, t2, t3;
+    float T;
+
+    scanf("%d %d %d", &t1, &t2, &t3);
+    T = t1 + t2 + t3;
+
+    printf("%.2f\n", T);
+
+    return 0;
+}
